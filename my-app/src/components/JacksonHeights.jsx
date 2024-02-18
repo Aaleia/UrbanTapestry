688f6c336d3e3d1bd57a49caa5a6e13c97dea53f
Iman Ikram <iman.ikram1122@gmail.com>
2024-02-17 19:23:49 -0500

    JacksonHeights page done

--- my-app/src/components/JacksonHeights.jsx
+++ my-app/src/components/JacksonHeights.jsx
@@ -1,29 +1,26 @@
 import React, { Component } from "react";
 import "./../components/Neighborhood.css";
+import img from "../assets/JH.webp"
 
 class JacksonHeights extends Component {
   render() {
     return (
       <div class="container">
-        <h1>Jackson Heights</h1>
-        <p>
-          Jackson Heights, nestled in the borough of Queens, New York City, has
-          a rich and diverse history that mirrors the waves of immigration that
-          have shaped the city. Originally farmland, the area experienced rapid
-          development in the early 20th century, particularly after the
-          Queensboro Bridge opened in 1909, facilitating easier access to
-          Manhattan. Initially designed as a planned community, Jackson Heights
-          attracted middle-class residents seeking suburban living with urban
-          conveniences. However, the neighborhood's demographic landscape
-          shifted dramatically in the mid-20th century with successive waves of
-          immigration, particularly from Latin America, South Asia, and other
-          parts of the world. This influx transformed Jackson Heights into one
-          of the most ethnically diverse neighborhoods in the city, with a
-          vibrant tapestry of cultures, languages, and traditions. Today,
-          Jackson Heights is celebrated for its multiculturalism, thriving
-          immigrant communities, and dynamic culinary scene, embodying the
-          spirit of New York City as a melting pot of cultures.
-        </p>
+       
+      <div className="image-container">
+          <img src={img} alt="China Town" style={{ width: "100%", height: "90%", opacity: 0.5}} />
+          <h1 style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", color: "navy", fontSize: "5em", textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}>Jackson Heights</h1>
+        </div>
+        <p>Jackson Heights, a vibrant neighborhood nestled in the borough of Queens, New York City, boasts a rich tapestry of history and culture. Originally developed in the early 20th century as a planned community, Jackson Heights became a haven for diverse immigrant populations seeking refuge and opportunity in the United States. Over the years, it has evolved into one of the most ethnically diverse neighborhoods in the city, known for its harmonious blend of cultures, languages, and traditions. Explore Jackson Heights, and you'll encounter:</p>
+      <ul style={{ listStyleType: "disc", marginLeft: "20px" }}>
+        <li>Tree-lined streets and historic pre-war architecture</li>
+        <li>Bustling commercial corridors offering diverse shopping experiences</li>
+        <li>Eclectic dining scene featuring authentic ethnic cuisine from around the world</li>
+        <li>Vibrant community events celebrating the neighborhood's multicultural heritage</li>
+        <li>Diversity Plaza, a central hub showcasing the neighborhood's cultural richness</li>
+        <li>A melting pot of cultures, languages, and traditions, reflecting the resilience and spirit of its residents</li>
+      </ul>
+
       </div>
     );
   }
