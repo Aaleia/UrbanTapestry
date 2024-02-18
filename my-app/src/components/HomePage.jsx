50321d960c6fa61c08d37679d10ddd5fb70b2626
anitaprova <104943181+anitaprova@users.noreply.github.com>
2024-02-17 22:14:16 -0500

    replace the text

--- my-app/src/components/HomePage.jsx
+++ my-app/src/components/HomePage.jsx
@@ -20,17 +20,26 @@ class HomePage extends React.Component {
             <section className="box">
               <header>
                 <h2>What is this?</h2>
-                <p>Explanation</p>
+                <p>
+                  We developed a vibrant cultural map tailored specifically for
+                  New York City, divided by its diverse neighborhoods. This
+                  resource is designed with young kids in mind, providing a
+                  platform for children to delve deeper into the rich tapestry
+                  of their city.
+                </p>
               </header>
             </section>
             <div className="row">
               <div className="col-6 col-12-narrower">
                 <section className="box">
-                  <h3>This is a featured feature</h3>
-                  <p>Text</p>
+                  <h3>Map</h3>
+                  <p>
+                    We utilized Leaflet for React to craft an interactive map
+                    showcasing the various neighborhoods of New York City.{" "}
+                  </p>
                   <ul className="actions">
                     <li>
-                      <a href="#" className="button">
+                      <a href="/map" className="button">
                         Learn More
                       </a>
                     </li>
@@ -39,8 +48,8 @@ class HomePage extends React.Component {
               </div>
               <div className="col-6 col-12-narrower">
                 <section className="box">
-                  <h3>Another featured feature</h3>
-                  <p>More text</p>
+                  <h3>Sign In</h3>
+                  <p>Users are able to login to the site using their Google accounts. </p>
                   <ul className="actions">
                     <li>
                       <a href="#" className="button">
